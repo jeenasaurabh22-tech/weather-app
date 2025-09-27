aae4d9d8e33a4e188a845d4ca9e967053ada2265
jeenasaurabh22-tech <jeenasaurabh22@gmail.com>
2025-09-27 18:51:07 +0530

    made few changes regarding live weather updates

--- weather.js
+++ weather.js
@@ -1,4 +1,5 @@
 base_URL="https://api.open-meteo.com/";
+live_URL="https://api.openweathermap.org/data/2.5/weather?";
 box=document.querySelector(".box");
 let container=document.querySelector(".container");
 let hourly=document.querySelectorAll(".hourly");
@@ -33,14 +34,16 @@ btn.addEventListener("click", async (event)=>{
 let long_input=document.querySelector(".long input");
 longitude=long_input.value;
 
-const  URL=`${base_URL}/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,wind_speed_10m,relative_humidity_2m`
+const  URL=`${live_URL}lat=${latitude}&lon=${longitude}&appid=507380f421eff9044c3f63658e1f4fab`;
 let response= await fetch(URL);
 let data=await response.json();
 console.log(data);
-let temp=data.hourly.temperature_2m[0];
-let humid=data.hourly.relative_humidity_2m[0];
-let wind_speed=data.hourly.wind_speed_10m[0];
-msg_temp.innerHTML=`<p>${temp}<sup>o</sup>C</p>`;
+let temp=data.main.temp;
+let humid=data.main.humidity;
+let wind_speed=data.wind.speed;
+temp-=273;
+let t1=parseFloat(temp.toFixed(2));
+msg_temp.innerHTML=`<p>${t1}<sup>o</sup>C</p>`;
 msg_humid.innerText=`${humid}% Humidity`;
 msg_wind.innerText=`${wind_speed}Km/hr wind `;
 
@@ -58,7 +61,6 @@ nextBtn.addEventListener("click",async (evt)=>{
     container.classList.add("hide");
     box.classList.add("box");
     box.classList.remove("hideMore");
-
     let lati_input=document.querySelector(".lati input");
     latitude=lati_input.value;
 let long_input=document.querySelector(".long input");
@@ -67,18 +69,31 @@ longitude=long_input.value;
 const  URL=`${base_URL}/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,wind_speed_10m,relative_humidity_2m`
 let response= await fetch(URL);
 let data=await response.json();
+console.log(data);
+    const now = new Date();
+  const currentHour = now.toISOString().slice(0, 13) + ":00"; 
+  let index = data.hourly.time.indexOf(currentHour);
+  
+  let y=index;
+  
+
+    
 for(let i=0;i<=12;i++){
-    hourly[i].innerText=data.hourly.time[i];
+    hourly[i].innerText=data.hourly.time[index++];
 }
+
+index=y;
 for(let i=0;i<=12;i++){
-    temp[i].innerText=data.hourly.temperature_2m[i];
+    temp[i].innerText=data.hourly.temperature_2m[index++];
 }
+index=y;
 for(let i=0;i<=12;i++){
     humidi[i].innerText=data.hourly.relative_humidity_2m
-[i];
+[index++];
 }
+index=y;
 for(let i=0;i<=12;i++){
-    windi[i].innerText=data.hourly.wind_speed_10m[i];
+    windi[i].innerText=data.hourly.wind_speed_10m[index++];
 }
 
 })
